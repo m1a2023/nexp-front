bffe768b6cc46c0482841c6078e70caea2b8a347
hellblade000 <yukhnin2013@gmail.com>
2024-07-19 20:07:11 +0500

    changed absoluteButton component; added graphic

--- src/app/components/absoluteButton.js
+++ src/app/components/absoluteButton.js
@@ -1,19 +1,23 @@
-import "@fortawesome/fontawesome-free/css/fontawesome.min.css";
+import { useState } from "react";
 
 export const TemplateAbsoluteButton = ({ styles }) => {
+  const [isAbsoluteBlock, setIsAbsoluteBlock] = useState(false);
+
+  function homePageAbsoluteBlock() {
+    setIsAbsoluteBlock((current) => !current);
+  }
+
   function AddExpensesBlock({ display }) {
     return (
       <div
-        className={`backdrop-blur-lg backdrop-brightness-95 w-full h-full ${display} justify-center items-center`}
+        className={`backdrop-blur-lg backdrop-brightness-95 w-full h-full ${display} justify-center items-center `}
       >
         <div className="w-[35%] h-[60%] bg-gray-200 rounded-3xl flex flex-col items-center text-black p-4">
           <h1>Add Expenses</h1>
           <button
             className="p-4 bg-[red] text-white rounded-3xl"
             onClick={() => {
-              document.querySelector("#addExpensesBlock").style.display =
-                "none";
-              document.querySelector("#addExpensesBtn").style.display = "flex";
+              homePageAbsoluteBlock();
             }}
           >
             Exit dialog window
@@ -25,44 +29,42 @@ export const TemplateAbsoluteButton = ({ styles }) => {
 
   return (
     <>
-      <div
-        id="addExpensesBlock"
-        className="absolute w-full h-full flex flex-row hidden"
-      >
-        <AddExpensesBlock display={"flex"} />
-      </div>
-      <div
-        id="addExpensesBtn"
-        className={
-          styles.addInfoBtn +
-          " absolute bottom-5 right-5 " +
-          styles.addExpensesBtn
-        }
-        onClick={() => {
-          document.querySelector("#addExpensesBlock").style.display = "flex";
-          document.querySelector("#addExpensesBtn").style.display = "none";
-        }}
-      >
-        <span>
-          {/* <i class="fa-solid fa-plus"></i> */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="38"
-            height="38"
-            color="bg-gray-200"
-            fill="none"
-          >
-            <path
-              d="M12 4V20M20 12H4"
-              stroke="currentColor"
-              stroke-width="2.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
-        </span>
-      </div>
+      {isAbsoluteBlock ? (
+        <div className="absolute w-full h-full flex flex-row">
+          <AddExpensesBlock display={"flex"} />
+        </div>
+      ) : (
+        <div
+          className={
+            styles.addInfoBtn +
+            " absolute bottom-5 right-5 " +
+            styles.addExpensesBtn
+          }
+          onClick={() => {
+            homePageAbsoluteBlock();
+          }}
+        >
+          <span>
+            {/* <i class="fa-solid fa-plus"></i> */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="38"
+              height="38"
+              color="bg-gray-200"
+              fill="none"
+            >
+              <path
+                d="M12 4V20M20 12H4"
+                stroke="currentColor"
+                stroke-width="2.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </span>
+        </div>
+      )}
     </>
   );
 };
--- src/app/components/graphic.js
+++ src/app/components/graphic.js
@@ -1,3 +1,6 @@
+import Chart from "chart.js/auto";
+import { useEffect } from "react";
+
 export const TemplateGraphic = ({ graphicTitle, mainInformation, color }) => {
   const GraphicTitle = ({ graphicTitle }) => {
     return (
@@ -11,6 +14,44 @@ export const TemplateGraphic = ({ graphicTitle, mainInformation, color }) => {
     return <div>{mainInformation}</div>;
   };
 
+  useEffect(() => {
+    const ctx = document.getElementById("myChart");
+
+    let chart = new Chart(ctx, {
+      type: "doughnut",
+      data: {
+        labels: [
+          "Supermarkets",
+          "Drug Store",
+          "Online stores",
+          "Online services",
+        ],
+        datasets: [
+          {
+            label: "Percent",
+            data: [43, 4, 26, 25],
+            backgroundColor: [
+              "rgb(255, 99, 132)",
+              "rgb(54, 162, 235)",
+              "rgb(255, 205, 86)",
+              "rgb(46, 98, 76)",
+            ],
+            hoverOffset: 4,
+          },
+        ],
+      },
+    });
+
+    // chart.destroy();
+  });
+
+  const Graphic = () => {
+    return (
+      <div className="chart-container w-[400px] h-[400px]">
+        <canvas id="myChart"></canvas>
+      </div>
+    );
+  };
   return (
     <div
       className={`flex flex-col w-3/4 px-6 py-4 ${color} text-black rounded-3xl shadow-2xl select-none`}
@@ -18,6 +59,8 @@ export const TemplateGraphic = ({ graphicTitle, mainInformation, color }) => {
       <GraphicTitle graphicTitle={`${graphicTitle}`} />
 
       <GraphicMainInformation mainInformation={`${mainInformation}`} />
+
+      <Graphic />
     </div>
   );
 };
