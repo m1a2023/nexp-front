1d6b4ce1508b52c5e8c28161630475b0e5caf173
m1a2023 <mag2017go@gmail.com>
2024-09-02 00:29:57 +0500

    refactored Home page; added SquareExpenseCHartBlock with some inner functions and constants;

--- src/app/develop/page.js
+++ src/app/develop/page.js
@@ -1,3 +1,9 @@
+import { SquareExpenseChartBlock } from "../components/squareExpenseChart";
+
 export default function Develop() {
-  return <div className="flex justify-center h-full w-full"></div>;
+  return (
+    <div className="flex justify-center h-full w-full">
+      <SquareExpenseChartBlock />
+    </div>
+  );
 }
