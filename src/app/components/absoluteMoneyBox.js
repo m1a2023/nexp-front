f9891e332dffd6b25cbc70527c9ba3cb6f786640
hellblade000 <yukhnin2013@gmail.com>
2024-09-06 13:16:40 +0500

    bug fix on home page

--- src/app/components/absoluteMoneyBox.js
+++ src/app/components/absoluteMoneyBox.js
@@ -7,7 +7,7 @@ import Image from "next/image";
 export const AllMoneyBoxTemplate = ({ styles }) => {
   const isAllMoneyBox = MoneyBoxStore((state) => state.isMoneyBox);
   const changeMoneyBoxState = MoneyBoxStore(
-    (state) => state.changeMoneyBoxState,
+    (state) => state.changeMoneyBoxState
   );
 
   return (
